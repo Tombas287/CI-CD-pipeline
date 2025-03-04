887e0088461af82709dd2dd8ed297c82f61d41b1
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-04 16:43:29 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,7 +2,7 @@ def call(String dockerImage) {
     echo "Pushing Docker image to registry: ${dockerImage}"
     
     // Log in to Docker registry
-    withCredentials([usernamePassword(credentialsId: 'docker-credentials-id', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker_login', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
     }
     
