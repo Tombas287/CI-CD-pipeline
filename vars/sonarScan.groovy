699891f7abff117892f82c5c8a6c7e5989b512c0
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-04 22:44:02 +0530

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -7,7 +7,7 @@
  */
 def call(Map config = [:]) {
     def projectKey = config.projectKey ?: error("Missing required parameter: projectKey")
-    def sonarHost = config.sonarHost ?: 'http://localhost:9000'
+    def sonarHost = config.sonarHost ?: 'http://host.docker.internal:9000'
     def sonarToken = config.sonarToken ?: error("Missing required parameter: sonarToken")
 
     script {
