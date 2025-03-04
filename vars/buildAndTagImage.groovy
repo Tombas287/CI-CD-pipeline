51bab656e93b817e1b0eac0e779621b194262a1a
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-04 15:50:06 +0530

    Update buildAndTagImage.groovy

--- vars/buildAndTagImage.groovy
+++ vars/buildAndTagImage.groovy
@@ -1,4 +1,6 @@
 def call(String dockerTag) {
     echo "Building Docker image and tagging it as: ${dockerTag}"
-    sh 'docker build -t ${dockerTag} .'
+    sh "docker build -t ${dockerTag} ."
+}
+
 }
