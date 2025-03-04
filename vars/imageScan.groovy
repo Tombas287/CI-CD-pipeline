07edafcc7a6f6928061dbd9d56725612c1d46e6c
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-04 21:46:04 +0530

    Update imageScan.groovy

--- vars/imageScan.groovy
+++ vars/imageScan.groovy
@@ -1,7 +1,7 @@
 def call(String dockerImage){
     echo "Running Trivy image scan on: ${dockerImage}"
     sh """
-    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --quiet --exit-code 0 --severity HIGH,CRITICAL ${dockerImage}
+    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image --quiet --exit-code 0 --severity HIGH,CRITICAL ${dockerImage}
 
     """
 
