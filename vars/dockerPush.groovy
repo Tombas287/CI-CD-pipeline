d292729b2a6e90eb86897cda39e744aa6c4cae93
Maxwell134 <maxbasumatry@gmail.com>
2025-03-04 16:36:59 +0530

    added tag

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,11 @@
+def call(String dockerImage) {
+    echo "Pushing Docker image to registry: ${dockerImage}"
+    
+    // Log in to Docker registry
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials-id', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
+    }
+    
+    // Push the Docker image
+    sh "docker push ${dockerImage}"
+}
