ae9ca5f26fe695a950d842db7b672259c77e92c8
Maxwell134 <maxbasumatry@gmail.com>
2025-03-11 22:53:37 +0530

    added image

--- vars/sampleText.groovy
+++ vars/sampleText.groovy
@@ -23,3 +23,26 @@ def call(filePath) {
         }
     }
 }
+
+def checkImage(filePath) {
+    def fileContent = readFile(filePath).trim()
+    def jsonslurper = new JsonSlurper()
+    def jsonObj = jsonslurper.parseText(fileContent)
+
+    def imageName = jsonObj.imageName
+    def imageTag = jsonObj.imageTag
+
+    if (!imageName?.trim() || !imageTag?.trim()) {
+        echo "❌ Missing imageName or imageTag in JSON file."
+        return false  // ✅ Return false if JSON is invalid
+    }
+
+    def status = sh(script: "curl -s -f https://hub.docker.com/v2/repositories/${imageName}/tags/${imageTag}", returnStatus: true)
+    if (status == 0) {
+        echo "✅ Image exists in Docker Hub."
+        return true
+    } else {
+        echo "❌ Image not found in Docker Hub."
+        return false
+    }
+}
