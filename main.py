3f72144cb8e3feb821c3fc23aa4919af4744279f
Maxwell134 <maxbasumatry@gmail.com>
2025-03-13 23:51:17 +0530

    added

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def health():
     return jsonify("Health is fine"), 201
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080,host='0.0.0.0')
+    app.run(debug=True, port=8080, host='0.0.0.0')
 
 
