8caec2c409686dd436fbc4d8b5a0ab27cc47c2af
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-15 19:05:00 +0530

    Update AKSdeployer.groovy

--- vars/AKSdeployer.groovy
+++ vars/AKSdeployer.groovy
@@ -11,8 +11,8 @@ def call(String environment, String credentials, String dockerImage , String ima
             helm version
             """
             // Check if image exists
-            def jsonslurper = new JsonSlurperClassic()
             def configFile = readFile(pipeline)
+            def jsonslurper = new JsonSlurperClassic()
             def jsonObj = jsonslurper.parseText(configFile)
             def finalImage = dockerImage ?: jsonObj.imageName
             def finalTag = imageTag ?: jsonObj.imageTag
