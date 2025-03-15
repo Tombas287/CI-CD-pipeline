332e726554b0fa9aed1bdda8aaaf54e9b2a35805
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-16 00:17:01 +0530

    Update blueGreenDeployment.groovy

--- vars/blueGreenDeployment.groovy
+++ vars/blueGreenDeployment.groovy
@@ -1,36 +1,41 @@
-def getHelmReleaseVersions(String releaseName , String namespace) {
-    def historyJson = sh(script: "helm history ${releaseName} -n ${namespace} -o json", returnStdout:true).trim()
+def getHelmReleaseVersions(String releaseName, String namespace) {
+    // Fetch Helm release history in JSON format
+    def historyJson = sh(script: "helm history ${releaseName} -n ${namespace} -o json", returnStdout: true).trim()
     return readJSON(text: historyJson)
-
 }
 
 def rollbackHelm(String releaseName, String namespace, int version) {
+    // Perform Helm rollback to the specified version
     sh "helm rollback ${releaseName} ${version} -n ${namespace}"
-
 }
 
-def selectHelmVersion(String releaseName, String namespace ) {
+def selectHelmVersion(String releaseName, String namespace) {
+    // Fetch Helm release history
     def history = getHelmReleaseVersions(releaseName, namespace)
+    
+    // Check if history is empty
     if (history.isEmpty()) {
         error "No Helm releases found for ${namespace}"
     }
-    
+
+    // Display available Helm versions
     println "Available Helm Versions:"
-    history.each { entry -> }
+    history.each { entry ->
         println "[Release name: ${entry.name} -- ${entry.revision}] Deployed on: ${entry.updated} - Status: ${entry.status}"
-        
-      }
-      def selectedVersion = input (
-         message: "Select Helm version  for rollback",
-         parameters : [
-         choice(name: 'Versions', choices: history.collect {it.revision as String }, description: "choose the Helm version to  rollback")
-                
-            ]
-         )   
+    }
+
+    // Prompt user to select a version for rollback
+    def selectedVersion = input(
+        message: "Select Helm version for rollback",
+        parameters: [
+            choice(name: 'Versions', choices: history.collect { it.revision as String }, description: "Choose the Helm version to rollback")
+        ]
+    )
+
     return selectedVersion.toInteger()
 }
 
-
+// Example usage
 def selectedVersion = selectHelmVersion(releaseName, namespace)
 println("Rolling back to version: ${selectedVersion}")
 rollbackHelm(releaseName, namespace, selectedVersion)
