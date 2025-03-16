cfb9150c0e311fd959a87c04f42663eacec7096d
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-16 22:53:16 +0530

    Update AKSdeployer.groovy

--- vars/AKSdeployer.groovy
+++ vars/AKSdeployer.groovy
@@ -67,9 +67,9 @@ def deploy(String environment, String image, String tag) {
         """
         resourceQuota("my-quota", environment)
         def releaseName = "my-app-release-${environment}-myrelease"
-        sh "kubectl  get pod  -n dev"
+        // sh "kubectl  get pod  -n dev"
         // blueGreenDeployment(releaseName, environment)
-        // blueGreenDeployment("my-app-release-${environment}", environment)
+        blueGreenDeployment("my-app-release-${environment}", environment)
     } catch (Exception e) {
         echo "❌ Deployment failed for ${environment}. Rolling back..."
         // rollbackHelm(environment)
