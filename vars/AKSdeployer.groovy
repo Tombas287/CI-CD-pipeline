f5b428b4f54d0aaa0174c7d8268285e0cd07eb0b
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2025-03-16 21:54:23 +0530

    Update AKSdeployer.groovy

--- vars/AKSdeployer.groovy
+++ vars/AKSdeployer.groovy
@@ -64,7 +64,7 @@ def deploy(String environment, String image, String tag) {
         """
         resourceQuota("my-quota", environment)
         def releaseName = "my-app-release-${environment}-myrelease"
-        blueGreenDeployment(releaseName )
+        blueGreenDeployment(releaseName, environment)
         // blueGreenDeployment("my-app-release-${environment}", environment)
     } catch (Exception e) {
         echo "❌ Deployment failed for ${environment}. Rolling back..."
